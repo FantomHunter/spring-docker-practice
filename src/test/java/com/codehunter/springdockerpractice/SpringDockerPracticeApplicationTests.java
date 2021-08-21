687d6080c0ad42404f41e136cee881c119fb8716
AAVN\hvhai <m.hovanhai@gmail.com>
2021-08-21 09:31:59 +0700

    first demo

---
+++ src/test/java/com/codehunter/springdockerpractice/SpringDockerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehunter.springdockerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
