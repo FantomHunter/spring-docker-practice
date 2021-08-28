85b413b12764fac0c538dd74988a04626798ad5a
AAVN\hvhai <m.hovanhai@gmail.com>
2021-08-28 16:53:36 +0700

    change response to the JSON type

--- src/main/java/com/codehunter/springdockerpractice/controllers/GreetingController.java
+++ src/main/java/com/codehunter/springdockerpractice/controllers/GreetingController.java
@@ -2,6 +2,8 @@ package com.codehunter.springdockerpractice.controllers;
 
 import com.codehunter.springdockerpractice.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.NumberUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,23 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class GreetingController {
     private final UserRepository userRepository;
 
-
     @GetMapping("/greeting")
-    public String greeting() {
-        return "Hello docker v2: build fat jar from src";
+    public GreetingResponse greeting() {
+        log.info("Say Greeting");
+        return new GreetingResponse("Hello docker v2: build fat jar from src");
     }
 
     @GetMapping("/greeting-from-db")
-    public String greetingFromDb(@RequestParam String id) {
-
-        if (id != null){
+    public GreetingResponse greetingFromDb(@RequestParam String id) {
+        if (id != null) {
+            log.info("greetingFromDb for user "+ id);
             Long idAsLong = Long.parseLong(id);
-            return userRepository.findById(idAsLong).map(userDAO -> userDAO.getName()).orElse("id not found");
+            return userRepository.findById(idAsLong)
+                    .map(userDAO -> new GreetingResponse(userDAO.getName()))
+                    .orElse(new GreetingResponse("id not found"));
         }
-        return "id is required";
+        return new GreetingResponse("id is required");
+    }
 
+    @Data
+    @AllArgsConstructor
+    public class GreetingResponse {
+        private String message;
     }
 }
